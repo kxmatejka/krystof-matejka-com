d63549e8eb9313b3ef8e46ec0bcd8b53617ebdb1
Kryštof Matějka <krystof.matejka@pipedrive.com>
2023-08-05 16:11:54 +0200

    init page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,40 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
+type PostProps = {
+  title: string
+  tags: string[]
+  youtube: string
+}
+
+function Post({title, tags, youtube}: PostProps) {
+  return (
+    <div className={styles.postBody}>
+      <h3 className={styles.postTitle}>{title}</h3>
+      <ul className={styles.postTags}>
+        {tags.map((tag, i) => (<li key={i}>{tag}</li>))}
+      </ul>
+      <iframe width="100%" height="315" src={youtube} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+  )
+}
+
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div>
+      <h1 className={styles.pageTitle}>Krystof Matejka</h1>
+      <h2 className={styles.pageSubtitle}>Site reliability engineer in the streets, data analyst in the sheets</h2>
+      <Post
+        title='Outreach - scallable front-ends'
+        tags={['english', 'talk', 'front-end', 'monitoring', '2023']}
+        youtube='https://www.youtube.com/embed/6D40yXg6PtQ'
+      />
+      <Post
+        title='Betsys - devops vs sre'
+        tags={['czech', 'podcast', 'devops', 'sre', '2023']}
+        youtube='https://www.youtube.com/embed/6D40yXg6PtQ'
+      />
       </div>
     </main>
   )
