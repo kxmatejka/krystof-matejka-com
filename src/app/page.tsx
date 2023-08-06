15c5e94eb80b2e89b9e221826a7c5512d81b4291
Kryštof Matějka <krystof.matejka@pipedrive.com>
2023-08-06 14:57:59 +0200

    fix responsive desing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import { Attachment, AttachmentProps } from './components/atomics/attachment'
 import styles from './page.module.css'
 
 type PostProps = {
-  title: string
-  tags: string[]
-  attachment: AttachmentProps
+	title: string
+	tags: string[]
+	attachment: AttachmentProps
 }
 
 function Post({ title, tags, attachment }: PostProps) {
@@ -14,7 +14,7 @@ function Post({ title, tags, attachment }: PostProps) {
 			<ul className={styles.postTags}>
 				{tags.map((tag, i) => (<li key={i}>{tag}</li>))}
 			</ul>
-			<Attachment {...attachment}/>
+			<Attachment {...attachment} />
 		</div>
 	)
 }
@@ -22,40 +22,38 @@ function Post({ title, tags, attachment }: PostProps) {
 export default function Home() {
 	return (
 		<main className={styles.main}>
-			<div>
-				<h1 className={styles.pageTitle}>Krystof Matejka</h1>
-				<h2 className={styles.pageSubtitle}>Site reliability engineer in the streets, data analyst in the sheets</h2>
-				<Post
-					title='Betsys - devops vs sre'
-					tags={['czech', 'podcast', 'devops', 'sre', '2023']}
-					attachment={{
-						type: 'youtube',
-						previewUrl: '/images/2023-betsys.webp',
-						contentUrl: 'https://www.youtube.com/watch?v=ShBAmqwEYNw',
-						description: 'Betsys - devops vs sre'
-					}}
-				/>
-				<Post
-					title='Outreach - scalable front-end'
-					tags={['english', 'presentation', 'front-end', 'monitoring', '2023']}
-					attachment={{
-						type: 'pdf',
-						previewUrl: '/images/2023-front-end-monitoring.webp',
-						contentUrl: '/slides/2023-front-end-monitoring.pdf',
-						description: 'slides for scalable front-end meetup'
-					}}
-				/>
-				<Post
-					title='Zelení - Pardubice z opozice'
-					tags={['czech', 'talk', 'green party', 'environment', 'local politics', '2023']}
-					attachment={{
-						type: 'youtube',
-						previewUrl: '/images/2023-zeleni.webp',
-						contentUrl: 'https://www.youtube.com/watch?v=npMphHuY1NY',
-						description: 'pardubice z opozice'
-					}}
-				/>
-			</div>
+			<h1 className={styles.pageTitle}>Krystof Matejka</h1>
+			<h2 className={styles.pageSubtitle}>Site reliability engineer in the streets, data analyst in the sheets</h2>
+			<Post
+				title='Betsys - devops vs sre'
+				tags={['czech', 'podcast', 'devops', 'sre', '2023']}
+				attachment={{
+					type: 'youtube',
+					previewUrl: '/images/2023-betsys.webp',
+					contentUrl: 'https://www.youtube.com/watch?v=ShBAmqwEYNw',
+					description: 'Betsys - devops vs sre'
+				}}
+			/>
+			<Post
+				title='Outreach - scalable front-end'
+				tags={['english', 'presentation', 'front-end', 'monitoring', '2023']}
+				attachment={{
+					type: 'pdf',
+					previewUrl: '/images/2023-front-end-monitoring.webp',
+					contentUrl: '/slides/2023-front-end-monitoring.pdf',
+					description: 'slides for scalable front-end meetup'
+				}}
+			/>
+			<Post
+				title='Zelení - Pardubice z opozice'
+				tags={['czech', 'talk', 'green party', 'environment', 'local politics', '2023']}
+				attachment={{
+					type: 'youtube',
+					previewUrl: '/images/2023-zeleni.webp',
+					contentUrl: 'https://www.youtube.com/watch?v=npMphHuY1NY',
+					description: 'pardubice z opozice'
+				}}
+			/>
 		</main>
 	)
 }
